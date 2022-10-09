e692d7e6207cf68759ef555d4aa46cc713039ba2
Osman Erdem Kılıç <osman_erdem1200@hotmail.com>
2022-10-09 11:34:52 +0300

    Disney Plus

---
+++ data.js
@@ -0,0 +1,32 @@
+let movies = [
+    {
+        name: 'falcon and the winter soldier',
+        des: 'Sam Wilson ve Bucky Barnes yeni ve tanıdık düşmanlarla yüzleşmek için bir araya gelmelidir. Falcon ve Winter Soldier, onları teste tabi tutan küresel bir macerada bir araya geliyor.',
+        image: 'images/slider 2.PNG'
+    },
+
+    {
+        name: 'loki',
+        des: 'Loki, kendisinin ve MCU nun geleceğini sonsuza dek değiştiren gizemi ortaya çıkarmalıdır. Yaramazlık Tanrısı, Marvel Studios un orijinal dizisinde geri döndü.',
+        image: 'images/slider 1.PNG'
+    },
+    {
+        name: 'wanda vision',
+        des: "WandaVision'a hoş geldiniz. Hiçbir şeyin göründüğü gibi olmadığı yer. Yalnızca Disney+'ta. Wanda Maximoff ve Vision. Ne güzel bir çift. Disney+'ta onların hikayelerini keşfet.",
+        image: 'images/slider 3.PNG'
+
+    },
+
+    {
+        name: 'raya and the last dragon',
+        des: "Raya and The Last Dragon, Lumandra'yı kurtarmak için son ejderhanın izini süren yalnız bir savaşçı olan Raya'nın hikayesini konu ediyor.",
+        image: 'images/slider 4.PNG'
+
+    },
+
+    {
+        name: 'luca',
+        des: "Luca'nın yeni arkadaşı okyanusun altında bulunan başka bir dünyada gelen deniz canavarıdır.",
+        image: 'images/slider 5.PNG'
+    }
+];
